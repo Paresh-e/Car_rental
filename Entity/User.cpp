92c32bdb349aa92d5544c885e32a1f1b1afc3060
Paresh-e <eshraghyparsa@gmail.com>
2026-02-09 14:50:32 +0330

    Default User needs

--- Entity/User.cpp
+++ Entity/User.cpp
@@ -1,5 +1,20 @@
-//
-// Created by asus on 2/9/2026.
-//
-
 #include "User.h"
+
+User::User()
+{
+    id = 0;
+    role = GUEST;
+    blocked = false;
+}
+
+User::User(int id,
+           const std::string& username,
+           const std::string& passwordHash,
+           UserRole role)
+{
+    this->id = id;
+    this->username = username;
+    this->passwordHash = passwordHash;
+    this->role = role;
+    this->blocked = false;
+}
--- Entity/User.h
+++ Entity/User.h
@@ -4,10 +4,27 @@
 
 #ifndef CAR_RENTAL_USER_H
 #define CAR_RENTAL_USER_H
-
+#include <string>
+enum UserRole
+{
+    GUEST,
+    CUSTOMER,
+    STAFF,
+    MANAGER
+};
 
 class User {
-
+public :
+    int id;
+    std::string username;
+    std::string passwordHash;
+    UserRole role;
+    bool blocked;
+    User();
+    User(int id,
+         const std::string& username,
+         const std::string& passwordHash,
+         UserRole role);
 };
 
 

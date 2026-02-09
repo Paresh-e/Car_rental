f58004031a91e6956cb325f9483a2b012a76f816
Paresh-e <eshraghyparsa@gmail.com>
2026-02-09 15:00:29 +0330

    Default Car needs

--- Entity/Car.cpp
+++ Entity/Car.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Car.h"
+int Car::IDcounter = 0 ;
+Car::Car()
+{
+    id = ++IDcounter;
+    pricePerDay = 0;
+    status = AVAILABLE;
+}
+
+Car::Car(int id,
+         const std::string& brand,
+         const std::string& type,
+         double pricePerDay)
+{
+    this->id = id;
+    this->brand = brand;
+    this->type = type;
+    this->pricePerDay = pricePerDay;
+    this->status = AVAILABLE;
+}
--- Entity/Car.h
+++ Entity/Car.h
@@ -4,11 +4,30 @@
 
 #ifndef CAR_RENTAL_CAR_H
 #define CAR_RENTAL_CAR_H
-
-
-class Car {
-
+#include <string>
+enum CarStatus
+{
+    AVAILABLE,
+    RESERVED,
+    RENTED,
+    MAINTENANCE
 };
+class Car
+{
 
+public:
+    static int IDcounter;
+    int id;
+    std::string brand;
+    std::string type;
+    double pricePerDay;
+    CarStatus status;
+
+    Car();
+    Car(int id,
+        const std::string& brand,
+        const std::string& type,
+        double pricePerDay);
+};
 
 #endif //CAR_RENTAL_CAR_H

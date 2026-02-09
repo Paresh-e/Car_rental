914a57cfe782d4d10d08245560f85163820cfc58
Paresh-e <eshraghyparsa@gmail.com>
2026-02-09 17:02:51 +0330

    Menu is here

---
+++ Menu/Menu.cpp
@@ -0,0 +1,23 @@
+//
+// Created by asus on 2/9/2026.
+//
+
+#include "Menu.h"
+#include <iostream>
+using namespace std;
+void Menu::start() {
+    cout<<"------------------------------"<<endl;
+    cout<<"Welcome..||CarRentalSystem||"<<endl;
+    cout
+    <<"1.Guest\n"
+    <<"2.Customer\n"
+    <<"3.Staff\n"
+    <<"4.Maintenace\n"
+    <<"5.Manager"
+    <<endl;
+
+
+
+
+
+}

b4bf0b34480066824e453b26ef285cceee8af778
Paresh-e <eshraghyparsa@gmail.com>
2026-02-09 17:12:30 +0330

    All panel Created

---
+++ Panels/ManagerPanel.h
@@ -0,0 +1,14 @@
+//
+// Created by asus on 2/9/2026.
+//
+
+#ifndef CAR_RENTAL_MANAGERPANEL_H
+#define CAR_RENTAL_MANAGERPANEL_H
+
+
+class ManagerPanel {
+
+};
+
+
+#endif //CAR_RENTAL_MANAGERPANEL_H
---
+++ Panels/StaffPanel.h
@@ -0,0 +1,14 @@
+//
+// Created by asus on 2/9/2026.
+//
+
+#ifndef CAR_RENTAL_STAFFPANEL_H
+#define CAR_RENTAL_STAFFPANEL_H
+
+
+class StaffPanel {
+
+};
+
+
+#endif //CAR_RENTAL_STAFFPANEL_H

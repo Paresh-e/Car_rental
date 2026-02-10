07c51dfc3329694c9d8924fe4388c5e521280b93
Paresh-e <eshraghyparsa@gmail.com>
2026-02-11 00:58:47 +0330

    Maintenance manager is completed

---
+++ Managers/MaintenanceManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by asus on 2/11/2026.
+//
+
+#include "MaintenanceManager.h"
---
+++ Managers/MaintenanceManager.h
@@ -0,0 +1,14 @@
+//
+// Created by asus on 2/11/2026.
+//
+
+#ifndef CAR_RENTAL_MAINTENANCEMANAGER_H
+#define CAR_RENTAL_MAINTENANCEMANAGER_H
+
+
+class MaintenanceManager {
+
+};
+
+
+#endif //CAR_RENTAL_MAINTENANCEMANAGER_H

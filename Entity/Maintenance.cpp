1d277ca797ac418cd1c923309fcc8e94f2b76e41
Paresh-e <eshraghyparsa@gmail.com>
2026-02-11 00:31:25 +0330

    Maintenance Default needs completed

--- Entity/Maintenance.cpp
+++ Entity/Maintenance.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Maintenance.h"
+
+Maintenance::Maintenance() {
+
+}
+
+Maintenance::Maintenance(Date & En, Date &Ex, string &Des, double Co, int CarId, int UserID) {
+    this->Entry = En;
+    this->Exit= Ex;
+    this->Description = Des;
+    this->Cost= Co;
+    this->CarId = CarId;
+    this->UserId = UserID;
+
+}
+
--- Entity/Maintenance.h
+++ Entity/Maintenance.h
@@ -6,7 +6,18 @@
 #define CAR_RENTAL_MAINTENANCE_H
 
 
+#include "Date.h"
+
 class Maintenance {
+public:
+    Date Entry;
+    Date Exit;
+    string Description;
+    double Cost;
+    int CarId;
+    int UserId;
+    Maintenance();
+    Maintenance(Date&,Date&,string&,double,int,int);
 
 };
 

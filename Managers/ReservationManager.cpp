b2d95d13acc7ef05b0453a87bbc82878848b215a
Paresh-e <eshraghyparsa@gmail.com>
2026-02-10 20:08:03 +0330

    Load & Save completed

---
+++ Managers/ReservationManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by asus on 2/10/2026.
+//
+
+#include "ReservationManager.h"
---
+++ Managers/ReservationManager.h
@@ -0,0 +1,14 @@
+//
+// Created by asus on 2/10/2026.
+//
+
+#ifndef CAR_RENTAL_RESERVATIONMANAGER_H
+#define CAR_RENTAL_RESERVATIONMANAGER_H
+
+
+class ReservationManager {
+
+};
+
+
+#endif //CAR_RENTAL_RESERVATIONMANAGER_H
